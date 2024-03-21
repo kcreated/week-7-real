a54a5db1dcf7dad6df3600ab9f5949780ae4f308
kcreated <kyciabt@gmail.com>
2024-03-21 14:50:27 -0400

    heh

--- Grades.java
+++ Grades.java
@@ -1,6 +1,40 @@
-Grades.Java
+/***********
+ * Programmer: Kate Chae
+ * Date: March 21st
+ * Program Name:Grades
+ *********/
+import java.util.*;
+public class Grades
 
-Ask the user for their Grade
+{
+   public static void main(String[] args)
+   {
+      int grade;
 
-if(grade>90) 
-"Impressive, you got an A"
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter a number grade: ");
+      grade = input.nextInt();
+      input.close();
+
+      if(grade >= 90) 
+      {
+         System.out.println("Impressive, you got an A");
+      }
+      else if(grade >= 80) 
+      {
+         System.out.println("Your average self got a B");
+      }
+      else if(grade >= 70) 
+      {
+         System.out.println("You somehow got a C");
+      }
+      else if(grade >= 60)
+      {
+         System.out.println("Ofcourse you got a D");
+      }
+      else
+      {
+         System.out.println("Impressive, you got an F");
+      }
+   }
+}			
